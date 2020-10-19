36030e59b2e7d0a0895acc501029dc54944ebcc9
tdl <582713429@qq.com>
2020-10-19 13:47:20 +0800

    fixed：swagger添加token输入

--- exam/src/main/java/com/jf/exam/Swagger2.java
+++ exam/src/main/java/com/jf/exam/Swagger2.java
@@ -4,13 +4,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import  org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import  springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import  springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //swagger2的配置文件，在项目的启动类的同级文件建立
 @Configuration
@@ -21,12 +26,17 @@ public class Swagger2 {
     // swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("令牌{Bearer token}").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 // 为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("com.jf.exam.controller")).paths(PathSelectors.any())
-                .build();
+                .build().globalOperationParameters(pars);
     }
     // 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
     private ApiInfo apiInfo() {
